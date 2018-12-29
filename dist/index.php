1a785aa1d816cbf6122231305425e7f72a7d95d1
Hidehiko Naito <hidehiko@7110.email>
2018-12-29 12:14:51 +0900

    add index.php

---
+++ dist/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>front-env PHP | 7110</title>
+</head>
+<body>
+
+<?php
+echo "wrap html module by php";
+?>
+
+</body>
+</html>
